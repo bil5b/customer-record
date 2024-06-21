5db8a554e6977f0a475348f9e426d100840a0bbd
Bilal Saeed Bhatti <bilalsaeedb@gmail.com>
2024-06-21 07:49:47 +0500

    Fix problem with production environment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
@@ -17,7 +18,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/customers', customerRoutes);
 
-app.get('/', (req, res) => res.send('Server ready'))
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server ready'))
+    }
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
